38044774b555dff7b70525928ea2cbfd60dd77b8
heihei <raions.seibu@gmail.com>
2022-07-11 13:55:43 +0900

    ルーティング作成

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// ルーティング
+	// e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
+
+func getUser(c echo.Context) error {
+	// ユーザID (users/:id)
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
